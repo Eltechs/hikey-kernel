c102fd4ef0f80571fe6f2c82fabec0e2feba698d
Niranjan Yadla <nyadla@cadence.com>
2017-08-16 16:30:39 -0700

    hifi-dsp: Send mailbox messages from ap to dsp
    Implemented hifi mailbox to send messaged from ap to dsp
    
    Signed-off-by: Niranjan Yadla <nyadla@cadence.com>

--- drivers/hisi/hifi_dsp/hifi_om.c
+++ drivers/hisi/hifi_dsp/hifi_om.c
@@ -723,12 +723,100 @@ static const struct file_operations hifi_debug_proc_ops = {
 	.write = hifi_debug_level_store,
 };
 
+#define HIKEY_MSG_HEAD_PROTECT_WORD 0xffff1234
+#define HIKEY_MSG_BODY_PROTECT_WORD 0xffff4321
+
+#define HIKEY_AP2DSP_MSG_QUEUE_ADDR HIFI_HIKEY_SHARE_ADDR
+#define HIKEY_AP2DSP_MSG_QUEUE_SIZE 0x1800
+#define HIKEY_DSP2AP_MSG_QUEUE_ADDR (HIKEY_AP2DSP_MSG_QUEUE_ADDR + HIKEY_AP2DSP_MSG_QUEUE_SIZE)
+#define HIKEY_DSP2AP_MSG_QUEUE_SIZE 0x1800
+
+struct hikey_ap2dsp_msg_head {
+	unsigned int head_protect_word;
+	unsigned int msg_num;
+	unsigned int read_pos;
+	unsigned int write_pos;
+};
+
+struct hikey_ap2dsp_msg_body {
+	unsigned short msg_id;
+	unsigned short msg_len;
+	char msg_content[0];
+};
+
+static struct hikey_ap2dsp_msg_head *msg_head;
+static void hikey_init_share_mem(char *share_mem_addr,
+				 unsigned int share_mem_size)
+{
+	if (!share_mem_addr) {
+		loge("share memory is null\n");
+		return;
+	}
+
+	memset(share_mem_addr, 0, share_mem_size);
+	msg_head = (struct hikey_ap2dsp_msg_head *)share_mem_addr;
+	msg_head->head_protect_word = HIKEY_MSG_HEAD_PROTECT_WORD;
+	msg_head->msg_num = 0;
+	msg_head->read_pos = (unsigned int)sizeof(struct hikey_ap2dsp_msg_head);
+	msg_head->write_pos = msg_head->read_pos;
+}
+
+static void hikey_ap2dsp_write_msg(struct hikey_ap2dsp_msg_body *hikey_msg)
+{
+	unsigned int size_to_bottom = 0;
+	unsigned int write_size = 0;
+
+	loge("Enter %s\n", __func__);
+	if (!msg_head) {
+		loge("hikey share memory not init\n");
+		return;
+	}
+
+	if (!hikey_msg) {
+		loge("msg is null\n");
+		return;
+	}
+
+	if (msg_head->head_protect_word != HIKEY_MSG_HEAD_PROTECT_WORD) {
+		loge("hikey msg head protect word error,0x%x\n",
+		     msg_head->head_protect_word);
+		return;
+	}
+
+	write_size = hikey_msg->msg_len + sizeof(struct hikey_ap2dsp_msg_body);
+	size_to_bottom = (HIKEY_DSP2AP_MSG_QUEUE_SIZE - msg_head->write_pos);
+
+	if (write_size >= HIKEY_DSP2AP_MSG_QUEUE_SIZE) {
+		loge("msg is too long\n");
+		return;
+	}
+
+	if (size_to_bottom > write_size) {
+		memcpy((char *)((char *)msg_head + msg_head->write_pos),
+		       hikey_msg, write_size);
+		msg_head->write_pos += write_size;
+	} else {
+		memcpy((char *)((char *)msg_head + msg_head->write_pos),
+		       hikey_msg, size_to_bottom);
+		memcpy((char *)((char *)msg_head +
+				sizeof(struct hikey_ap2dsp_msg_head)),
+		       (char *)hikey_msg + size_to_bottom,
+		       write_size - size_to_bottom);
+		msg_head->write_pos =
+		    sizeof(struct hikey_ap2dsp_msg_head) + (write_size -
+							    size_to_bottom);
+	}
+
+	msg_head->msg_num++;
+	loge("Exit %s\n", __func__);
+}
+
 static int hifi_send_str_todsp(const char *cmd_str, size_t size)
 {
 	int ret = OK;
 	unsigned int msg_len = 0;
 	hifi_str_cmd *pcmd = NULL;
-
+	struct hikey_ap2dsp_msg_body *hikey_msg = NULL;
 	BUG_ON(cmd_str == NULL);
 
 	msg_len = sizeof(hifi_str_cmd) + size + 1;	/*add 1 for last \0 */
@@ -743,7 +831,9 @@ static int hifi_send_str_todsp(const char *cmd_str, size_t size)
 	pcmd->msg_id = ID_AP_AUDIO_STR_CMD;
 	pcmd->str_len = size;
 	strncpy(pcmd->str, cmd_str, size);
+	hikey_msg = (struct hikey_ap2dsp_msg_body *)pcmd;
 
+	hikey_ap2dsp_write_msg(hikey_msg);
 	ret =
 	    (int)mailbox_send_msg(MAILBOX_MAILCODE_ACPU_TO_HIFI_MISC, pcmd,
 				  msg_len);
@@ -895,25 +985,6 @@ void *memcpy_aligned(void *_dst, const void *_src, unsigned len)
 	return _dst;
 }
 
-static int read_hifi_shared_addr(void)
-{
-	unsigned int *read_hifi_shared_addr = NULL;
-	int ret = 0;
-
-	loge(" Enter %s\n", __func__);
-	read_hifi_shared_addr = ioremap_wc(0x8B300000, 50);
-	if (read_hifi_shared_addr == NULL) {
-		loge(" %s(): read_hifi_shared_addr ioremap_wc failed\n",
-		     __func__);
-	} else {
-		loge(" 0x8B300000:0x%x \n", readl(read_hifi_shared_addr));
-		iounmap(read_hifi_shared_addr);
-		read_hifi_shared_addr = NULL;
-	}
-	loge(" %s():Exit\n", __func__);
-	return ret;
-}
-
 int load_hifi_img_by_misc(void)
 {
 	unsigned int i = 0;
@@ -977,8 +1048,8 @@ int load_hifi_img_by_misc(void)
 			}
 			memcpy_aligned((void *)(iomap_dest_addr),
 				       (void *)((char *)hifi_img +
-						hifi_img->sections[i].
-						src_offset),
+						hifi_img->
+						sections[i].src_offset),
 				       hifi_img->sections[i].size);
 			iounmap(iomap_dest_addr);
 		}
@@ -1002,7 +1073,6 @@ static ssize_t hifi_dsp_reset_option_show(struct file *file, char __user *buf,
 	}
 	if (load_hifi_img_by_misc() == 0) {
 		g_om_data.dsp_loaded = true;
-		read_hifi_shared_addr();
 		loge("g_om_data.dsp_loaded:%d\n", (int)g_om_data.dsp_loaded);
 	}
 	snprintf(reset_option, RESET_OPTION_LEN,
@@ -1333,6 +1403,10 @@ void hifi_om_init(struct platform_device *pdev,
 
 	*(g_om_data.dsp_exception_no) = ~0;
 	g_om_data.pre_exception_no = ~0;
+	hikey_init_share_mem((char *)(hifi_priv_base_virt +
+				      (HIFI_HIKEY_SHARE_MEM_ADDR -
+				       HIFI_UNSEC_BASE_ADDR)),
+			     (unsigned int)HIFI_HIKEY_SHARE_SIZE);
 	g_om_data.dsp_fama_config->head_magic = DRV_DSP_SOCP_FAMA_HEAD_MAGIC;
 	g_om_data.dsp_fama_config->flag = DRV_DSP_FAMA_OFF;
 	g_om_data.dsp_fama_config->rear_magic = DRV_DSP_SOCP_FAMA_REAR_MAGIC;
@@ -1449,8 +1523,8 @@ void hifi_om_effect_mcps_info_show(struct hifi_om_effect_mcps_stru
 		    ID_EFFECT_ALGO_BUTT
 		    && hifi_mcps_info->effect_mcps_info[i].effect_algo_id >
 		    ID_EFFECT_ALGO_START) {
-			switch (hifi_mcps_info->
-				effect_mcps_info[i].effect_stream_id) {
+			switch (hifi_mcps_info->effect_mcps_info[i].
+				effect_stream_id) {
 			case AUDIO_STREAM_PCM_OUTPUT:
 				logw("Algorithm: %s, Mcps: %d, Stream: PCM_OUTPUT \n", effect_algo[hifi_mcps_info->effect_mcps_info[i].effect_algo_id - 1].effect_name, hifi_mcps_info->effect_mcps_info[i].effect_algo_mcps);
 				break;
